8ee94c89838f03cf6d76f787bff8190b80259f6c
yal1412 <71146838+yal1412@users.noreply.github.com>
2022-02-28 23:11:43 +0300

    Add files via upload

---
+++ src/main/java/ru/sbt/edu/concurrency/counter/ConcurrentCounter.java
@@ -0,0 +1,28 @@
+package ru.sbt.edu.concurrency.counter;
+import java.util.concurrent.*;
+
+public class ConcurrentCounter implements Counter{
+    private long count;
+    Semaphore semaphore;
+
+    public ConcurrentCounter() {
+        this.semaphore = new Semaphore(1, true);
+        this.count = 0;
+    }
+
+    @Override
+    public void increment() {
+        try {
+            semaphore.acquire();
+            count++;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        semaphore.release();
+    }
+
+    @Override
+    public long getValue() {
+        return count;
+    }
+}
